44b0ad79119e88c24122a4b71f1fb82f04294328
mudrhs1997 <mudrhs1997@daum.net>
2023-01-17 19:03:43 +0900

    Commad 1.0.0

---
+++ Commad/Commad/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Commad
+//
+//  Created by 정명곤 on 2023/01/17.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .link
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
